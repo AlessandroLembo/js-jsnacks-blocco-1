0d7ed1f9352bce63375ea2a64da1cd5eb59456b6
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-13 18:04:02 +0100

    modifica analisi e creazione array

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -13,8 +13,24 @@ Comunicagli se può partecipare o no alla festa.
 ANALISI
 1 - Creare un array con i nomi degli invitati.
 2 - Prendere gli elementi dalla pagina.
-3 - Inserisco il nome dell'utente.
-4 - Controllo se è stato invitato alla festa.
-5 - Stampo in pagina il messaggio-
+3 - Collego il button a un event listener.
+4 - Inserisco il nome dell'utente e lo salvo in una variabile.
+5 - Effettuo un primo controllo per verficare se ho inserito qualcosa nell'input.
+6 - Dichiaro una variabile a cui assegno un valore booleano.
+7 - Controllo se l'utente è stato invitato alla festa.
+8 - Stampo in pagina il messaggio.
 
 */
+
+// 1 - Creare un array con i nomi degli invitati.
+const listParticipant = ['Ale', 'Marco', 'Edo', 'Giulia', 'Anna'];
+// console.log(listParticipant);
+
+
+// 2 - Prendere gli elementi dalla pagina.
+const participantElement = document.getElementById('participant');
+const checkUserElement = document.getElementById('check-user');
+const messageElement = document.getElementById('message');
+// console.log(participantElement, checkUserElement, messageElement);
+
+
