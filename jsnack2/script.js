532fef796f4999c5f86f8b19b01fd814d9b07b48
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-13 18:34:53 +0100

    modifica analisi e stampa del messaggio in pagina

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -17,8 +17,9 @@ ANALISI
 4 - Inserisco il nome dell'utente e lo salvo in una variabile.
 5 - Effettuo un primo controllo per verficare se ho inserito qualcosa nell'input.
 6 - Dichiaro una variabile a cui assegno un valore booleano.
-7 - Controllo se l'utente è stato invitato alla festa.
-8 - Stampo in pagina il messaggio.
+7 - Dichiaro un variabile in cui salvo un messaggio per i non invitati.
+8 - Controllo se l'utente è stato invitato alla festa.
+9 - Stampo in pagina il messaggio.
 
 */
 
@@ -33,4 +34,33 @@ const checkUserElement = document.getElementById('check-user');
 const messageElement = document.getElementById('message');
 // console.log(participantElement, checkUserElement, messageElement);
 
+// 3 - Collego il button a un event listener
+checkUserElement.addEventListener('click', function() {
+     
+    // 4 - Inserisco il nome dell'utente e lo salvo
+    const nameParticipant = participantElement.value.trim();
 
+    // 5 - Controllo se ho cliccato lasciando il campo vuoto
+    if (!nameParticipant) {
+        messageElement.innerText = "Inserisci un nome prima di cliccare";
+        return;
+    } 
+
+    // 6e7 - Dichiaro 2 varabili.
+    let isInvited = false;
+    let messageAccess = "Non è stato invitato";
+
+    // 8 - Controllo se l'utente è stato invitato alla festa.
+    for (let i = 0; i < listParticipant.length && !isInvited; i++) {
+        const currentUser = listParticipant[i];
+
+        if (currentUser === nameParticipant) {
+            isInvited = true;
+            messageAccess = "Puo' entrare alla festa";
+        }
+    }
+
+    // 9 - Stampo in pagina il messaggio
+    messageElement.innerText = messageAccess;
+ 
+})
