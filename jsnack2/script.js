e2e131bb0e44a4b6bd3f49dd31afbfdf015de107
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-13 17:40:06 +0100

    snack 2 - analisi e scrittura HTML

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -8,3 +8,13 @@ In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
 l’utente inserisce il suo nome tramite un input. 
 Comunicagli se può partecipare o no alla festa. 
 */
+
+/*
+ANALISI
+1 - Creare un array con i nomi degli invitati.
+2 - Prendere gli elementi dalla pagina.
+3 - Inserisco il nome dell'utente.
+4 - Controllo se è stato invitato alla festa.
+5 - Stampo in pagina il messaggio-
+
+*/
