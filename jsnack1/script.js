341d194aee13f9b875f37c49db0f4648bc6a6d3e
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-02 17:54:07 +0100

    Inserimento ciclo while e creazione array

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -12,37 +12,43 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 ANALISI
 
 1 - Prendo l'elemento dalla pagina.
-2 - Dichiaro una variabile in cui andrò a inserire la somma dei numeri.
-3 - Chiedere all'utente 10 volte di inserire un numero.
-4 - Calcolare la somma dei numeri inseriti.
-5 - Stampare in pagina.
+2 - Array vuoto che andrà a contenere 10 numeri inseiriti dall'utente.
+3 - Dichiaro una variabile in cui andrò a inserire la somma dei numeri.
+4 - Dichiaro una variabile per la validazione.
+5 - Chiedere all'utente di inserire 10 numeri.
+6 - Calcolare la somma dei numeri inseriti.
+7 - Stampare in pagina.
 */
 
 // 1 - Prendo l'elemento dalla pagina.
 const sumElement = document.getElementById('sum');
 
+// 2 - Array vuoto che andrà a contenere 10 numeri inseiriti dall'utente.
+const numbersSum = [];
 
-// 2 - Dichiaro una variabile in cui andrò a inserire la somma dei numeri.
-let numbersSum = 0;
+// 3 - Dichiaro una variabile in cui andrò a inserire la somma dei numeri.
+let result = 0;
 
+// 4 - Dichiaro una variabile per la validazione.
 let isValid = true;
 
-/* 3e4 - Chiedo all'utente 10 volte di inserire un numero
+/* 5e6 - Chiedo all'utente di inserire 10 numeri
          e ne calcolo la somma */
-for (let i = 1; i <= 10; i++) {
-    const userNumbers = parseInt(prompt("Inserisci un numero").trim());
-    console.log(userNumbers);
-    if (isNaN(userNumbers)){
-        isValid = false;
-        alert('Hai inserito valori non validi');
-    } else {
-        numbersSum += userNumbers; 
-    } 
-      
-    
-}
+while (numbersSum.length < 10){
+  const userNumbers = parseInt(prompt('Inserisci un numero').trim());
+  console.log(userNumbers);
 
-// 5 - Stampa in pagina
-console.log(`La somma dei numeri è uguale a ${numbersSum}`);
-sumElement.innerText = `La somma dei numeri è uguale a ${numbersSum}`;
+  if (isNaN(userNumbers)){
+    isValid = false;
+  } else {
+    numbersSum.push(userNumbers);
+    result += userNumbers;
+  }
+  
+}
+    
+
+// 7 - Stampa in pagina
+console.log(`La somma dei numeri è uguale a ${result}`);
+sumElement.innerText = `La somma dei numeri è uguale a ${result}`;
 
